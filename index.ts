f825a88aaf83722c0badf025522d530073a930c9
Eman__Iqbal <160255293+EmanIqbal620@users.noreply.github.com>
2024-07-14 08:11:53 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// 1) computer will genetate a random number - Done.
+
+// 2) User input fir gussing number - Done
+
+// 3) computer user input with computer genetated number and show result
+
+
+const randonNumber = Math.floor(Math.random() *10+1);
+console.log(randonNumber);
+const answers = await inquirer.prompt([
+{
+  name: "userGuessedNumber",
+  type: "number",
+  message: "please guess a number between 1 - 10",
+},
+]);
+
+if(answers.userGuessNumber === randonNumber){
+  console.log("congtratulation! you gussed right number.")
+}else{
+  console.log("you guess wrong number");
+
+};
